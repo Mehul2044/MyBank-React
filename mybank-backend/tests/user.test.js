b02e2fbeb038e97896a2f3e8f697748564489ee4
Mehul Agarwal <mehu2003@gmail.com>
2024-05-01 23:26:48 +0530

    add test case

--- mybank-backend/tests/user.test.js
+++ mybank-backend/tests/user.test.js
@@ -1,22 +1,79 @@
 const supertest = require('supertest');
 const chai = require('chai');
-const {app} = require('../app'); // Import your app
+const {app} = require('../app');
 
 const expect = chai.expect;
 const request = supertest(app);
 
 describe('User Routes', function() {
+    // Existing test
     describe('POST /trackLogin', function() {
         it('should track login', function(done) {
             request.post('/user/trackLogin')
                 .send({ accountNumber: '123456' })
                 .end(function(err, res) {
                     expect(res.statusCode).to.equal(200);
-                    // Add more assertions as needed
                     done();
                 });
         });
     });
 
-    // Add more tests for other routes...
+    // New tests
+    describe('GET /getName', function() {
+        it('should get the user name', function(done) {
+            request.get('/user/getName')
+                .set('userToken', 'valid_token')
+                .end(function(err, res) {
+                    expect(res.statusCode).to.equal(200);
+                    expect(res.body).to.have.property('name');
+                    done();
+                });
+        });
+    });
+
+    describe('POST /login', function() {
+        it('should login the user', function(done) {
+            request.post('/user/login')
+                .send({ accountNumber: '123456', password: 'password' })
+                .end(function(err, res) {
+                    expect(res.statusCode).to.equal(200);
+                    expect(res.body).to.be.a('string');
+                    done();
+                });
+        });
+    });
+
+    describe('POST /register', function() {
+        it('should register a new user', function(done) {
+            request.post('/user/register')
+                .send({
+                    firstName: 'John',
+                    lastName: 'Doe',
+                    email: 'john.doe@example.com',
+                    phoneNumber: '1234567890',
+                    fileUrl: 'http://example.com/file.pdf'
+                })
+                .end(function(err, res) {
+                    expect(res.statusCode).to.equal(200);
+                    expect(res.body).to.be.a('string');
+                    done();
+                });
+        });
+    });
+
+    describe('POST /transfer', function() {
+        it('should transfer funds', function(done) {
+            request.post('/user/transfer')
+                .send({
+                    userToken: 'valid_token',
+                    recipientAccountNumber: 'recipient_account_number',
+                    amount: 100
+                })
+                .end(function(err, res) {
+                    expect(res.statusCode).to.equal(200);
+                    expect(res.body).to.have.property('message');
+                    done();
+                });
+        });
+    });
 });
